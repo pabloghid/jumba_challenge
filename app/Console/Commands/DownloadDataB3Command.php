6743c24afa1805f0b71094735630a053577ca7bf
pabloghid <pablodg290@gmail.com>
2023-05-23 11:48:51 -0300

    refactor: changed place to code

--- app/Console/Commands/DownloadDataB3Command.php
+++ app/Console/Commands/DownloadDataB3Command.php
@@ -2,9 +2,9 @@
 
 namespace App\Console\Commands;
 
-use App\Services\B3DataService;
+
 use Illuminate\Console\Command;
-use App\Helpers\DateHelper;
+use App\Jobs\DownloadDataB3Job;
 class DownloadDataB3Command extends Command
 {
     /**
@@ -26,45 +26,8 @@ class DownloadDataB3Command extends Command
      */
     public function handle()
     {
-        ## Informação de quantos dias
-        $pastDates = 10;
-        $dates = DateHelper::getPastDays($pastDates);
-
-        foreach ($dates as $date) {
-            $initial_url = 'https://arquivos.b3.com.br/api/download/requestname';
-            $params = ['fileName' => 'LendingOpenPosition', 'date' => $date];
-
-
-            $headers = get_headers($initial_url . '?' . http_build_query($params));
-
-            if(strpos($headers[0], '200') == false) {
-                continue;
-            }
-
-            // Faz o request inicial para pegar a url e o token
-            $response = file_get_contents($initial_url . '?' . http_build_query($params));
-            $data = json_decode($response, true);
-            
-            // Cria a URL com o token para download
-            $url = "https://arquivos.b3.com.br/api/download/?token=" . $data['token'];
-
-            $file = file_get_contents($url);
-            
-            $formattedDate = str_replace('-', '', $date);
-            $savePath = base_path() . "\\temp\LendingOpenPositionFile_$formattedDate.csv";
-            file_put_contents($savePath, $file);
-
-            ## Insere data no BD
-            $b3Service = new B3DataService();
-
-            $data = $b3Service->readCsv($formattedDate);
-
-            $insert = $b3Service->execute($data);
-
-            ## Exclui registros na pasta temp
-            $clearTemp = $b3Service->clearTemp();
-
-        }
+        $job = new DownloadDataB3Job;
+        $job->handle();
     }
 
 }
--- app/Jobs/DownloadDataB3Job.php
+++ app/Jobs/DownloadDataB3Job.php
@@ -9,7 +9,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Artisan;
+use App\Helpers\DateHelper;
+use App\Services\B3DataService;
 
 class DownloadDataB3Job implements ShouldQueue
 {
@@ -28,8 +29,45 @@ class DownloadDataB3Job implements ShouldQueue
      */
     public function handle(): void
     {
-        // Chama o comando para baixar os dados da B3
-        $command = new DownloadDataB3Command();
-        $command->handle();
+        ## Informação de quantos dias
+        $pastDates = 10;
+        $dates = DateHelper::getPastDays($pastDates);
+
+        foreach ($dates as $date) {
+            $initial_url = 'https://arquivos.b3.com.br/api/download/requestname';
+            $params = ['fileName' => 'LendingOpenPosition', 'date' => $date];
+
+
+            $headers = get_headers($initial_url . '?' . http_build_query($params));
+
+            if(strpos($headers[0], '200') == false) {
+                continue;
+            }
+
+            // Faz o request inicial para pegar a url e o token
+            $response = file_get_contents($initial_url . '?' . http_build_query($params));
+            $data = json_decode($response, true);
+            
+            // Cria a URL com o token para download
+            $url = "https://arquivos.b3.com.br/api/download/?token=" . $data['token'];
+
+            $file = file_get_contents($url);
+            
+            $formattedDate = str_replace('-', '', $date);
+            $savePath = base_path() . "\\temp\LendingOpenPositionFile_$formattedDate.csv";
+            file_put_contents($savePath, $file);
+
+            ## Insere data no BD
+            $b3Service = new B3DataService();
+
+            $data = $b3Service->readCsv($formattedDate);
+
+            $insert = $b3Service->execute($data);
+
+            ## Exclui registros na pasta temp
+            $clearTemp = $b3Service->clearTemp();
+
+        }
+
     }
 }
